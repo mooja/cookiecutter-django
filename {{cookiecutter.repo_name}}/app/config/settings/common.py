b449c3c9e16129d0e56f6d55add0519406714a4e
Max Shkurygin <max.atreides@gmail.com>
2016-04-24 19:20:16 -0400

    fixed app volume paths

--- {{cookiecutter.repo_name}}/app/config/settings/common.py
+++ {{cookiecutter.repo_name}}/app/config/settings/common.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import, unicode_literals
 import environ
 
 ROOT_DIR = environ.Path(__file__) - 3  # (/a/b/myfile.py - 3 = /)
-APPS_DIR = ROOT_DIR.path('app')
+APPS_DIR = ROOT_DIR
 
 env = environ.Env()
 
